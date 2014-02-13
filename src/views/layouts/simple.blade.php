7df3de2535b0b9e4c8e4b05d60a98541ca798708
Justin Hilles <justin@1011i.com>
2014-02-13 07:52:11 -0800

    add cdn files to template

--- src/views/layouts/simple.blade.php
+++ src/views/layouts/simple.blade.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <title>{{ Config::get('admin::config.title') }}</title>
+        {{ HTML::style('//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css') }}
+        {{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css') }}
+        {{ HTML::style('//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css') }}
         {{{ stylesheet_link_tag('assets/stylesheets/package') }}}
         @show
         <!-- Queue $ calls till jQuery loaded at bottom -->
